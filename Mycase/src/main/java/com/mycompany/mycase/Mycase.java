52efb683747280b8e1ffd127c33cf304f6ab0133
Ítalo A. F. D. Souza <itelosouza@gmail.com>
2024-11-12 14:33:05 -0300

    Inicio Projeto

---
+++ Mycase/src/main/java/com/mycompany/mycase/Mycase.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mycase;
+
+/**
+ *
+ * @author 15865502689
+ */
+public class Mycase {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
